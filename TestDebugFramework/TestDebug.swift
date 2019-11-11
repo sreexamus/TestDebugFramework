172dd10eb0b0d52e6297eda99cb2944a11831aba
Sreekanth Reddy <sreekanthreddy.iragamreddy@united.com>
2019-11-10 21:20:56 -0600

    For Testing Symbolic debug of framework
    For Testing Symbolic debug of framework

---
+++ TestDebugFramework/TestDebug.swift
@@ -0,0 +1,19 @@
+//
+//  TestDebug.swift
+//  TestDebugFramework
+//
+//  Created by sreekanth reddy iragam reddy on 11/10/19.
+//  Copyright © 2019 DebugModulesTest. All rights reserved.
+//
+
+import Foundation
+
+public class TestDebug {
+    public func getName() -> String {
+        return getAllNames()
+    }
+
+    func getAllNames() -> String {
+        return "Testing Debugging"
+    }
+}
