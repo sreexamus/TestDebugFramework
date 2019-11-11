da8f5c27ba4b163c60bf74d6006a442ca5cf0bbc
Sreekanth Reddy <sreekanthreddy.iragamreddy@united.com>
2019-11-10 22:32:47 -0600

    platform supports changed to iphoneos

---
+++ Carthage/Build/iOS/TestDebugFramework.framework/Headers/TestDebugFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestDebugFramework.h
+//  TestDebugFramework
+//
+//  Created by sreekanth reddy iragam reddy on 11/10/19.
+//  Copyright © 2019 DebugModulesTest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestDebugFramework.
+FOUNDATION_EXPORT double TestDebugFrameworkVersionNumber;
+
+//! Project version string for TestDebugFramework.
+FOUNDATION_EXPORT const unsigned char TestDebugFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestDebugFramework/PublicHeader.h>
+
+
